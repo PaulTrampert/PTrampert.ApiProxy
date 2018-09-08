997c9eac3201b2c042395cf8ceb4993f42d4ff97
Paul Trampert <paul.trampert@ptrampert.com>
2018-09-08 11:32:52 -0400

    Start adding ApiProxyConfig.

---
+++ PTrampert.ApiProxy.Test/ApiConfigTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace PTrampert.ApiProxy.Test
+{
+    public class ApiConfigTests
+    {
+        private ApiConfig subject;
+
+        [SetUp]
+        public void SetUp()
+        {
+            subject = new ApiConfig
+            {
+                BaseUrl = "https://example.com/some/route/"
+            };
+        }
+
+        [Test]
+        public void BaseUrlHasTrailingSlashTrimmed()
+        {
+            Assert.That(subject.BaseUrl, Is.EqualTo("https://example.com/some/route"));
+        }
+    }
+}
---
+++ PTrampert.ApiProxy/ApiProxyConfig.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PTrampert.ApiProxy
+{
+    public class ApiProxyConfig : Dictionary<string, ApiConfig>
+    {
+    }
+
+    public class ApiConfig
+    {
+        private string baseUrl;
+
+        public string BaseUrl
+        {
+            get => baseUrl.TrimEnd('/');
+            set => baseUrl = value;
+        }
+    }
+}
