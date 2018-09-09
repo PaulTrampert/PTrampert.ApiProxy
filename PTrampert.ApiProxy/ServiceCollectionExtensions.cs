dd56ce4c1136a6b2d90a2ef788e1f6767e8c7553
Paul Trampert <paul.trampert@ptrampert.com>
2018-09-08 23:07:42 -0400

    Setup asp.net core extensions to add ApiProxyController to an application.

---
+++ PTrampert.ApiProxy/ServiceCollectionExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+using PTrampert.ApiProxy;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddApiProxy(this IServiceCollection services, IConfiguration config)
+        {
+            services.Configure<ApiProxyConfig>(config);
+            return Common(services);
+        }
+
+        public static IServiceCollection AddApiProxy(this IServiceCollection services, Action<ApiProxyConfig> setupAction)
+        {
+            services.Configure<ApiProxyConfig>(setupAction);
+            return Common(services);
+        }
+
+        private static IServiceCollection Common(IServiceCollection services)
+        {
+            services.AddScoped<IAuthenticationBuilder, AuthenticationBuilder>();
+            if (!services.Any(sd => typeof(HttpClient).IsAssignableFrom(sd.ServiceType)))
+            {
+                services.AddSingleton<HttpClient>();
+            }
+
+            return services;
+        }
+    }
+}
