7d5558ec1bbccbe1268128f55580698617244e6e
Paul Trampert <paul.trampert@ptrampert.com>
2018-09-09 16:55:14 -0400

    Document config classes.

---
+++ PTrampert.ApiProxy/ApiConfig.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using PTrampert.ApiProxy.Authentication;
+
+namespace PTrampert.ApiProxy
+{
+    /// <summary>
+    /// The configuration for a proxied API.
+    /// </summary>
+    public class ApiConfig
+    {
+        private string baseUrl;
+
+        /// <summary>
+        /// The BaseUrl for a proxied API. This value is required, and should be a fully qualified URL.
+        /// </summary>
+        public string BaseUrl
+        {
+            get => baseUrl.TrimEnd('/');
+            set => baseUrl = value;
+        }
+
+
+        /// <summary>
+        /// The type name of the <see cref="IAuthentication"/> type to use for this api.
+        /// </summary>
+        /// <seealso cref="BasicAuthentication"/>
+        /// <seealso cref="UserBearerAuthentication"/>
+        public string AuthType { get; set; }
+
+        /// <summary>
+        /// Dictionary of props specific to the specified <see cref="AuthType"/>.
+        /// </summary>
+        public IDictionary<string, string> AuthProps { get; set; } = new Dictionary<string, string>();
+
+        /// <summary>
+        /// Collection of header keys to proxy down from the response.
+        /// </summary>
+        public IEnumerable<string> ResponseHeaders { get; set; } = new List<string>();
+
+        /// <summary>
+        /// Collection of header keys to proxy up into the request.
+        /// </summary>
+        public IEnumerable<string> RequestHeaders { get; set; } = new List<string>();
+    }
+}
--- PTrampert.ApiProxy/ApiProxyConfig.cs
+++ PTrampert.ApiProxy/ApiProxyConfig.cs
@@ -1,27 +1,15 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace PTrampert.ApiProxy
 {
+    /// <summary>
+    /// Dictionary of configured API's that the proxy can handle. Keys are compared with <see cref="StringComparer.OrdinalIgnoreCase"/>.
+    /// </summary>
     public class ApiProxyConfig : Dictionary<string, ApiConfig>
     {
-    }
-
-    public class ApiConfig
-    {
-        private string baseUrl;
-
-        public string BaseUrl
+        public ApiProxyConfig() : base(StringComparer.OrdinalIgnoreCase)
         {
-            get => baseUrl.TrimEnd('/');
-            set => baseUrl = value;
         }
-
-        public string AuthType { get; set; }
-
-        public IDictionary<string, string> AuthProps { get; set; } = new Dictionary<string, string>();
-
-        public IEnumerable<string> ResponseHeaders { get; set; } = new List<string>();
-
-        public IEnumerable<string> RequestHeaders { get; set; } = new List<string>();
     }
 }
