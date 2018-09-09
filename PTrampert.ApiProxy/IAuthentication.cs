acc82b3f8c1e1cc494e2e5afe4581711d2523994
Paul Trampert <paul.trampert@ptrampert.com>
2018-09-09 16:31:08 -0400

    Add documentation comments for IAuthentication.

--- PTrampert.ApiProxy/IAuthentication.cs
+++ PTrampert.ApiProxy/IAuthentication.cs
@@ -3,8 +3,20 @@ using System.Threading.Tasks;
 
 namespace PTrampert.ApiProxy
 {
+    /// <summary>
+    /// Interface for providing an authentication header for API calls.
+    /// </summary>
+    /// <remarks>
+    /// Implementations of <see cref="IAuthentication"/> must provide an <see cref="AuthenticationHeaderValue"/> when <see cref="GetAuthenticationHeader"/>
+    /// is called. A return value of <value>null</value> is valid if the header could not be generated, but the API request should still be attempted. Otherwise,
+    /// the call should throw.
+    /// </remarks>
     public interface IAuthentication
     {
+        /// <summary>
+        /// Generate an <see cref="AuthenticationHeaderValue"/> for a proxied API call.
+        /// </summary>
+        /// <returns>The <see cref="AuthenticationHeaderValue"/></returns>
         Task<AuthenticationHeaderValue> GetAuthenticationHeader();
     }
 }
